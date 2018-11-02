7966ceefb412b5461b190585a94b1120616d67db
kant1724 <sonseungbum@naver.com>
2018-11-02 11:29:55 +0900

    commit

--- src/main/resources/static/js/partner/partnerStepThree.js
+++ src/main/resources/static/js/partner/partnerStepThree.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		goNext();
 	});
 	locationObj = $('.location');
-	selectAddress1();	
+	selectAddress("");	
 	$('#content').fadeIn(500);
 });
 
@@ -14,24 +14,36 @@ function goNext() {
 	goPage(url);	
 }
 
+var currentDepth = 1;
+var codeArr = [];
 var codeNmArr = [];
 function setLocation() {
 	locationObj.empty();
 	for (var i = 0; i < codeNmArr.length; ++i) {
-		var loc1 = '<button id="" style="font-size: 11px; margin: 5px;" type="button" class="btn btn-primary loc1">' + codeNmArr[i] + '</button>'
+		var loc1 = '<button id="' + codeArr[i] + '" style="font-size: 11px; margin: 5px;" type="button" class="btn btn-primary loc1">' + codeNmArr[i] + '</button>'
 		locationObj.append(loc1);
 	}
 	$(".btn.btn-primary.loc1").click(function() {
-		$("#select_div").css('display', 'block');
+		if (currentDepth < 4) {
+			var grCode = $(this).prop('id');
+			selectAddress(grCode);
+			currentDepth += 1;
+		} else {
+			$("#select_div").css('display', 'block');
+			currentDepth = 1;
+		}
 	});
+	
 }
 
-function selectAddress1() {
+function selectAddress(grCode) {
 	var option = {};
 		option.url = '/dlFnGetLocCdnmList';
 		option.success = function(data, status, xhr) {
+			codeArr = [];
 			codeNmArr = [];
 			for (var i = 0; i < data.length; ++i) {
+				var code = data[i].CODE;
 				var codeNm = data[i].CODE_NM;
 				var depth = data[i].DEPTH;
 				var grCode = data[i].GR_CODE;
@@ -41,10 +53,11 @@ function selectAddress1() {
 				var dp2Cdnm = data[i].DP2_CDNM;
 				var dp3Code = data[i].DP3_CODE;
 				var dp3Cdnm = data[i].DP3_CDNM;
+				codeArr.push(code);
 				codeNmArr.push(codeNm);
 			}
 			setLocation();
 		};
-        option.data = {"grCode" : ""};
+        option.data = {"grCode" : grCode};
 	gf_ajax(option);
 }
