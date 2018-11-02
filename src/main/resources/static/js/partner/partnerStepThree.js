b471bbdfebc20d09c9236f3c816f693ea45098a9
kant1724 <sonseungbum83@gmail.com>
2018-11-02 19:48:58 +0900

    commit

--- src/main/resources/static/js/partner/partnerStepThree.js
+++ src/main/resources/static/js/partner/partnerStepThree.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
 	$("#select").click(function() {
 		$("#select_div").css('display', 'none');
 		currentDepth = 0;
+		addAddress();
+		if ($('.location-selected').css('display') == 'none') {
+			$('.location-selected').fadeIn(500);
+		}
 		selectAddress("");
 	});
 	locationObj = $('.location');
@@ -20,31 +24,55 @@ function goNext() {
 }
 
 var currentDepth = 0;
+var currentSi = '';
+var currentGu = '';
+var currentDong = '';
 var codeArr = [];
 var codeNmArr = [];
+
 function setLocation() {
 	locationObj.empty();
 	if (currentDepth < 3) {
 		for (var i = 0; i < codeNmArr.length; ++i) {
-			var loc1 = '<button id="' + codeArr[i] + '" style="font-size: 11px; margin: 5px;" type="button" class="btn btn-primary loc1">' + codeNmArr[i] + '</button>'
-			locationObj.append(loc1);
+			locationObj.hide();
+			var loc = '<button id="' + codeArr[i] + '" style="font-size: 11px; margin: 5px;" type="button" class="btn btn-primary loc1">' + codeNmArr[i] + '</button>'
+			locationObj.append(loc);
+			locationObj.fadeIn(200);
 		}
 	} else {
 		for (var i = 0; i < codeNmArr.length; ++i) {
-			var loc1 = '<label class="checkbox">' + codeNmArr[i] + '<input id="" type="checkbox"><span class="checkmark"></span></label>'
-			locationObj.append(loc1);
+			locationObj.hide();
+			var loc = '<label class="checkbox">' + codeNmArr[i] + '<input id="' + codeArr[i] + '" type="checkbox"><span class="checkmark"></span></label>'
+			locationObj.append(loc);
+			locationObj.fadeIn(200);
 		}
-		
 	}
 	$(".btn.btn-primary.loc1").click(function() {
 		if (currentDepth < 3) {
 			var grCode = $(this).prop('id');
+			if (currentDepth == 1) {
+				currentSi = $(this).text();
+			} else if (currentDepth == 2) {
+				currentGu = $(this).text();
+			}
 			selectAddress(grCode);
 		}
 	});
 	if (currentDepth == 3) {
 		$("#select_div").css('display', 'block');
 	}
+}
+
+function addAddress() {
+	var total = 0;	
+	var checkboxObj = $('.checkbox').children();
+	for (var i = 0; i < checkboxObj.length; ++i) {
+		if ($(checkboxObj[i]).is(":checked")) {
+			total += 1;
+		}
+	}
+	var tbody = '<tr><td>' + currentSi + '</td><td>' + currentGu + '</td><td>' + total + '개 동</td>';
+	$('#location_selected_table').append(tbody);
 }
 
 function selectAddress(grCode) {
